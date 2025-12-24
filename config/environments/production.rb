70a5cd54be0e6b36ebe927ade45a6a698dbc1d08
cyberneum <footcoderr@gmail.com>
2025-12-24 23:09:56 +0900

    fix: 이메일 인증 URL에 www 서브도메인 추가
    SSL 인증서가 www.routinefinders.life에만 적용되어 있어
    www 없는 URL 접근 시 ERR_SSL_PROTOCOL_ERROR 발생하는 문제 수정

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,7 +57,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
 
   # Set host to be used by links generated in mailer templates.
-  app_host = Rails.application.credentials.dig(:app_host) || ENV.fetch("APP_HOST", "routinefinders.life")
+  app_host = Rails.application.credentials.dig(:app_host) || ENV.fetch("APP_HOST", "www.routinefinders.life")
   config.action_mailer.default_url_options = { host: app_host, protocol: "https" }
 
   # Use Resend for email delivery in production
