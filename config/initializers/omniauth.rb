a850a637c9ef3db35bfc94fb62bc121b5f908617
wonyteria <wonseoklee0627@gmail.com>
2026-01-03 13:47:04 +0900

    Fix Threads API error 4476002 by requesting only supported fields

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -6,5 +6,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :threads,
            ENV.fetch("THREADS_CLIENT_ID", "dummy_client_id"),
            ENV.fetch("THREADS_CLIENT_SECRET", "dummy_client_secret"),
-           scope: "threads_basic"
+           scope: "threads_basic",
+           fields: [ :id, :username, :name, :threads_profile_picture_url ],
+           callback_url: "https://routinefinders.life/auth/threads/callback"
 end
