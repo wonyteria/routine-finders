1b3ca391ade612e12c1f6aef80aa7cba9c77a2c7
wonyteria <wonseoklee0627@gmail.com>
2026-01-31 18:53:54 +0900

    chore: 세션 유지 기간을 1년으로 연장하여 로그인 지속성 개선

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -5,9 +5,12 @@ if Rails.env.production?
   # 현재 www로만 강제 접속하고 있으므로 도메인 설정을 제거하여 쿠키 충돌을 방지합니다.
   Rails.application.config.session_store :cookie_store,
     key: "_routine_finders_session",
+    expire_after: 1.year,
     secure: true,
     httponly: true,
     same_site: :lax
 else
-  Rails.application.config.session_store :cookie_store, key: "_routine_finders_session"
+  Rails.application.config.session_store :cookie_store,
+    key: "_routine_finders_session",
+    expire_after: 1.year
 end
