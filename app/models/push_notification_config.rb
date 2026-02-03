1fb37b686b6b1bb0cddb233bc764af05bc883faf
wonyteria <wonseoklee0627@gmail.com>
2026-02-04 00:29:40 +0900

    chore: Update default push notification times for evening (19:00) and night (22:00)

--- app/models/push_notification_config.rb
+++ app/models/push_notification_config.rb
@@ -16,7 +16,7 @@ class PushNotificationConfig < ApplicationRecord
     find_or_create_by!(config_type: "evening_reminder") do |c|
       c.title = "🌆 루파 클럽 저녁 리마인더"
       c.content = "{{nickname}}님, 오늘 하루도 고생 많으셨어요! 저녁 식사 전, 남은 루틴들을 하나씩 체크하며 나를 챙겨볼까요?"
-      c.schedule_time = "18:30"
+      c.schedule_time = "19:00"
     end
   end
 
@@ -24,7 +24,7 @@ class PushNotificationConfig < ApplicationRecord
     find_or_create_by!(config_type: "night_check") do |c|
       c.title = "🌙 루파 클럽 밤의 기록"
       c.content = "{{nickname}}님, 오늘 성장을 기록하셨나요? 잊기 전에 루틴을 완료하고 평온한 밤을 맞이하세요!"
-      c.schedule_time = "22:30"
+      c.schedule_time = "22:00"
     end
   end
 end
