429d358bc2fad3daf5d89cea8da5a2d45a4eff2e
wonseok <wonseoklee0627@gmail.com>
2026-02-13 02:05:03 +0900

    Revert "Add user timezone support to fix date mismatch for international users"
    This reverts commit aa1c2ff778e7f5788875f9d51ef864ebdd246ee0.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,6 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
-  around_action :set_time_zone, if: :current_user
-
   # Changes to the importmap will invalidate the etag for HTML responses
   # Changes to the importmap will invalidate the etag for HTML responses
   stale_when_importmap_changes
